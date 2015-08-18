6fdd0c7e5bbd895b524b5c3960a52e1e742d627b
Fernando <fernandoruedaoliva@gmail.com>
2015-08-18 21:44:17 +0200

    trying add turbine

--- 20-fichaservice/src/main/java/com/javiermoreno/springcloud/IntegracionWebservices.java
+++ 20-fichaservice/src/main/java/com/javiermoreno/springcloud/IntegracionWebservices.java
@@ -5,15 +5,18 @@
  */
 package com.javiermoreno.springcloud;
 
-import com.fasterxml.jackson.databind.JsonNode;
-import com.fasterxml.jackson.databind.ObjectMapper;
 import java.io.IOException;
 import java.util.concurrent.Future;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.scheduling.annotation.Async;
 import org.springframework.scheduling.annotation.AsyncResult;
 import org.springframework.stereotype.Component;
 import org.springframework.web.client.RestTemplate;
+
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 
 /**
  *
@@ -33,6 +36,7 @@ public class IntegracionWebservices {
    
 
     @Async
+    @HystrixCommand(fallbackMethod = "defaultObtenerStockAsync")
     public Future<Integer> obtenerStockAsync(String referencia) throws IOException {
         // Atiende qué bonito: indicas el nombre del servicio y restTemplate es capaz de recuperar su @
         String stockURL = "http://stockservice/productos/" + referencia + "/stock";
@@ -44,10 +48,18 @@ public class IntegracionWebservices {
 
 
     @Async
+    @HystrixCommand(fallbackMethod = "defaultObtenerFichaCatalogoAsync")
     public Future<Producto> obtenerFichaCatalogoAsync(String referencia) {
         String catalogoURL = "http://catalogoservice/catalogo/referencias/" + referencia;
         Producto producto = restTemplate.getForObject(catalogoURL, Producto.class);
         return new AsyncResult<>(producto);
     }
     
+    public Future<Integer> defaultObtenerStockAsync(String referencia){
+    	return new AsyncResult<>(0);
+    }
+    
+    public Future<Producto> defaultObtenerFichaCatalogoAsync(String referencia) {
+    	return new AsyncResult<>(new Producto());
+    }
 }
