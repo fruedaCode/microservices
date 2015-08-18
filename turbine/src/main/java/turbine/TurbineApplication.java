1e2be45abc33ffc9a3ed02557dbf2da3a04a3bc7
Fernando <fernandoruedaoliva@gmail.com>
2015-08-18 22:22:28 +0200

    intento 1 hystrixdashboard

--- turbine/src/main/java/turbine/TurbineApplication.java
+++ turbine/src/main/java/turbine/TurbineApplication.java
@@ -1,24 +1,20 @@
 package turbine;
 
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.turbine.amqp.EnableTurbineAmqp;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.core.env.StandardEnvironment;
 
 /**
  * @author Spencer Gibb
  * @author Dave Syer
  */
-@Configuration
-@EnableAutoConfiguration
+@SpringBootApplication
 @EnableTurbineAmqp
 @EnableDiscoveryClient
 public class TurbineApplication {
 
 	public static void main(String[] args) {
-		boolean cloudEnvironment = new StandardEnvironment().acceptsProfiles("cloud");
-		new SpringApplicationBuilder(TurbineApplication.class).web(!cloudEnvironment).run(args);
+		new SpringApplicationBuilder(TurbineApplication.class).run(args);
 	}
 }
