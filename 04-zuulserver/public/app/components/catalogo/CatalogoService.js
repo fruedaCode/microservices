0fe75474286f6e231ae8902eb4f5469e5193a5bb
fro59 <fernandoruedaoliva@gmail.com>
2015-10-14 18:11:00 +0200

    aplicacion angular recuperada

--- 04-zuulserver/public/app/components/catalogo/CatalogoService.js
+++ 04-zuulserver/public/app/components/catalogo/CatalogoService.js
@@ -1,6 +1,6 @@
 angular.module('tiendaAngular.components.catalogo.services', [])
 	.service('CatalogoService', function($http){
-		var baseUrl = "http://localhost:8000/productosinterno/productosInterno";
+		var baseUrl = "http://localhost:8000/productosservice/getProductosComposite";
 		this.getProductos = function(){
 			return  $http.get(baseUrl);
 		};
