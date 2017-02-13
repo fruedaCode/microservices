a8a5fb17a8a2631f817db9cdd36677eedf336688
Fernando <fernando.rueda@hexad.es>
2017-02-13 15:58:06 +0100

    metricas turbine funcionando

--- 20-fichaservice/src/main/java/com/javiermoreno/springcloud/Application.java
+++ 20-fichaservice/src/main/java/com/javiermoreno/springcloud/Application.java
@@ -4,8 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.context.annotation.Bean;
 import org.springframework.scheduling.annotation.EnableAsync;
-
+import org.springframework.web.client.RestTemplate;
 
 
 @SpringBootApplication
@@ -16,5 +18,11 @@ public class Application {
     
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
+    }
+
+    @LoadBalanced
+    @Bean
+    RestTemplate restTemplate() {
+        return new RestTemplate();
     }
 }
--- turbine/src/main/java/com/javiermoreno/springcloud/TurbineServer.java
+++ turbine/src/main/java/com/javiermoreno/springcloud/TurbineServer.java
@@ -1,27 +1,14 @@
 package com.javiermoreno.springcloud;
 
-import org.springframework.amqp.rabbit.connection.CachingConnectionFactory;
-import org.springframework.amqp.rabbit.connection.ConnectionFactory;
-import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.cloud.netflix.turbine.amqp.EnableTurbineAmqp;
-import org.springframework.context.annotation.Bean;
+import org.springframework.cloud.netflix.turbine.EnableTurbine;;
 
 @SpringBootApplication
-@EnableDiscoveryClient
-@EnableTurbineAmqp
+@EnableTurbine
+@EnableAutoConfiguration
 public class TurbineServer {
-
-    @Value("${app.rabbitmq.host:localhost}")
-    String rabbitMqHost;
-
-    @Bean
-    public ConnectionFactory connectionFactory() {
-        CachingConnectionFactory connectionFactory = new CachingConnectionFactory(rabbitMqHost);
-        return connectionFactory;
-    }
 
     public static void main(String[] args) {
         new SpringApplicationBuilder(TurbineServer.class).run(args);
